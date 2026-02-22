b97f377c2e7442e439f0aacc6291fcb2d274539d
Vinit Yadav <vinityk2004@gmail.com>
2026-02-22 17:36:23 +0530

    final push,required changes are made for hosting

--- index.js
+++ index.js
@@ -30,3 +30,5 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
